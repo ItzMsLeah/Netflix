847a1142c3432f04159259a886d75b041e47e133
Leah <139161011+ItzMsLeah@users.noreply.github.com>
2024-09-12 01:40:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// script.js
-
-// Exemplo de validação de formulário
 document.getElementById('login-form').addEventListener('submit', function(event) {
     let email = document.querySelector('input[name="email"]').value;
     let password = document.querySelector('input[name="password"]').value;
 
     if (!email || !password) {
         alert("Por favor, preencha todos os campos.");
-        event.preventDefault(); // Previne o envio do formulário
+        event.preventDefault();
     }
 });
